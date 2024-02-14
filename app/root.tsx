f7acc8fde126b121e2b188607ec3165011ca401e
alexveebee <alex100.askels@gmail.com>
2024-02-14 22:38:30 +0000

    init project

--- app/root.tsx
+++ app/root.tsx
@@ -7,9 +7,16 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
 } from "@remix-run/react";
 
+import indexCss from "./style/index.css";
+import flexCss from "./style/flex.css";
+
 export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: indexCss },
+  { rel: "stylesheet", href: flexCss },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
@@ -27,6 +34,82 @@ export default function App() {
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError() ;
+  return (
+    <html>
+      <head>
+        <title>Oops!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body className="rooterror flex" style={{
+      }}>
+        <div
+          style={{
+          }}
+        >
+        </div>
+        <div style={{
+          position: "relative",
+          display: "flex",
+          flexDirection: "column",
+          gap: "12px",
+          padding: "12px",
+          paddingTop: "24px",
+          width: "100%",
+          // backgroundColor: "#623030",
+          // border: "1px solid #7f3232",
+          // borderRadius: "24px",
+        }}>
+          <h2 style={{
+            textOverflow: "ellipsis",
+            overflow: "hidden",
+          }} className="noselect">
+            {isRouteErrorResponse(error)
+              ? `${error.status} ${error.statusText}`
+              : error instanceof Error
+              ? error.message
+              : "Unknown Error"}
+          </h2>
+          {
+            // @ts-expect-error
+            error.stack && (
+              <pre style={{ whiteSpace: "pre-wrap" }} className="stack">
+                {/* @ts-expect-error */}
+                { error.stack ?? "No stacktrace available." }
+              </pre>
+            )
+          }
+        </div>
+        <style>
+          {`
+            pre {
+              overflow-x: auto;
+            }
+            pre.stack {
+              background: #623030;
+              padding: 12px;
+              border-radius: 12px;
+              border: 1px solid #7f3232;
+              text-color: #fff;
+              overflow-x: auto;
+            }
+
+            @media (max-width: 600px) {
+              .body {
+                display: flex;
+                flex-direction: row;
+              }
+            }
+          `}
+        </style>
+        <Scripts />
       </body>
     </html>
   );
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,77 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "DriveHoster" },
+    { name: "description", content: "A Self Hosted File hosting service" },
   ];
 };
 
+export const loader = async () => {
+  try {
+    const ping = await fetch("http://localhost:6090/api/ping");
+    return {
+       ping: await ping.text(),
+       running: true
+    };
+  } catch (e) {
+    return {
+      ping: "Server is not running",
+      running: false
+    };
+  }
+}
+
 export default function Index() {
+  const { ping, running } = useLoaderData<{ ping: string, running: number}>();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="main-page">
+      <div className="side-panel side-left">
+        <div className="header-container">
+          <h1>DriveHoster</h1>
+        </div>
+        <div className="content-container">
+        </div>
+      </div>
+      <main>
+        {
+          !running && <div className="error-container">
+            <p>Server is not running</p>
+          </div>
+        }
+        {
+          running && (<><div className="header-container">
+            <div className="flex flex-gap">
+              <button className="btn-primary">
+                Login
+              </button>
+              <button className="btn">
+                Sign Up
+              </button>
+            </div>
+          </div>
+          <div className="content-container">
+            <p>A Self Hosted File hosting service</p>
+            <p>Server Status: {ping || "Loading..."}</p>
+          </div></>)
+        
+        }
+      </main>
     </div>
+
+    // <div id="main-container" style={{
+    //   display: "flex",
+    //   flexDirection: "column",
+    //   alignItems: "center",
+    //   justifyContent: "center",
+    //   height: "100vh",
+    //   gap: "12px",
+    // }}>
+    //   <h1>DriveHoster</h1>
+    //   <p>A Self Hosted File hosting service</p>
+    //   <p>Server Status: {ping || "Loading..."}</p>
+    // </div>
   );
 }
